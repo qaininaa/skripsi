6514d7212b3c37436869f2f455fc0ab445ba72e0
Karina Ghaisani <karinaghsn@gmail.com>
2026-03-28 08:22:29 +0700

    Redirect root route to login page instead of welcome view

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 // Redirect /dashboard berdasarkan role
