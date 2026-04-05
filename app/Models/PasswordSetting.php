e6245d1f5adfc5df41767af9b912396ce661a952
Karina Ghaisani <karinaghsn@gmail.com>
2026-04-06 01:28:25 +0700

    feat: add PasswordSetting model for managing password settings

--- app/Models/PasswordSetting.php
+++ app/Models/PasswordSetting.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class SystemSetting extends Model
+class PasswordSetting extends Model
 {
     protected $fillable = ['key', 'value'];
 
