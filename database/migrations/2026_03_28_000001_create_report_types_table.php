c1e860ccf4638398eb5d7f3d5313aef15ec41b2f
Karina Ghaisani <karinaghsn@gmail.com>
2026-04-22 20:43:45 +0700

    feat: Make SOP code and version fields mandatory in ReportType migration

--- database/migrations/2026_03_28_000001_create_report_types_table.php
+++ database/migrations/2026_03_28_000001_create_report_types_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
             $table->uuid('id')->primary();
             $table->string('name');
             $table->integer('annex_number');
-            $table->string('sop_code')->nullable();
-            $table->string('sop_version')->nullable();
+            $table->string('sop_code');
+            $table->string('sop_version');
             $table->timestamps();
         });
     }
