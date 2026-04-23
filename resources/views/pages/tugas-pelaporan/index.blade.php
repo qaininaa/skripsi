cd94cbe7f99aafc48d6e7f02ff6ad1ea46ad63af
Karina Ghaisani <karinaghsn@gmail.com>
2026-04-23 09:47:37 +0700

    feat: Add view-button component and replace inline view link in tugas-pelaporan index

--- resources/views/pages/tugas-pelaporan/index.blade.php
+++ resources/views/pages/tugas-pelaporan/index.blade.php
@@ -87,13 +87,7 @@
 
                             {{-- Tombol lihat, edit & hapus --}}
                             <td class="px-4 py-3 text-right whitespace-nowrap">
-                                <a href="{{ route('admin.laporan.preview', $item) }}"
-                                   class="inline-flex items-center gap-1 px-3 py-1.5 rounded-lg border border-gray-200 text-xs font-medium text-gray-600 hover:bg-gray-50 transition-colors">
-                                    <svg class="w-3.5 h-3.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" /><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                                    </svg>
-                                    Lihat
-                                </a>
+                                <x-buttons.view-button href="{{ route('tugas-pelaporan.edit', $item) }}" />
                                 <x-buttons.edit-button onclick="window.location.href='{{ route('tugas-pelaporan.edit', $item) }}'" />
                                     <x-buttons.delete-button
                                         :action="route('tugas-pelaporan.destroy', $item)"
