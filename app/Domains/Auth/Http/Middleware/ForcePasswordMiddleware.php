9117b5dbf25de0a180c02a6a906cd2aa8a4ddcde
Karina Ghaisani <karinaghsn@gmail.com>
2026-05-06 14:06:45 +0700

    fix: rename ForcePasswordChangeMiddleware to ForcePasswordMiddleware for consistency

--- app/Domains/Auth/Http/Middleware/ForcePasswordMiddleware.php
+++ app/Domains/Auth/Http/Middleware/ForcePasswordMiddleware.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class ForcePasswordChangeMiddleware
+class ForcePasswordMiddleware
 {
     protected array $except = [
         'password.change',
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'role' => \App\Domains\Auth\Http\Middleware\RoleMiddleware::class,
-            'password.check' => \App\Domains\Auth\Http\Middleware\ForcePasswordChangeMiddleware::class,
+            'password.check' => \App\Domains\Auth\Http\Middleware\ForcePasswordMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
